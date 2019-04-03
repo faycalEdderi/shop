59f7b854636a90baa3629c86b480c24a61585bb4
sebastien <sebastiendor@hotmail.fr>
2019-04-03 18:34:16 +0200

    photos page annexes

--- config/bundles.php
+++ config/bundles.php
@@ -2,20 +2,17 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
 
-=======
->>>>>>> 79b32495600e51356fc8e7bc7e71e5e4ff3aea2d
+
+
+
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-<<<<<<< HEAD
 
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 
-=======
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
->>>>>>> 79b32495600e51356fc8e7bc7e71e5e4ff3aea2d
+
 ];
